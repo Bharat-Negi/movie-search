b20e71bb9996ba8172f1240cde59de24559174e2
Bharat Singh Negi <bharat81183@gmail.com>
2025-12-05 13:59:59 +0530

    new file up

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import Movies from "./component/Movies"
-
+import {Routes, Route} from "react-router-dom"
+import Home from "./component/Home"
+import SingleMovie from "./component/SingleMovie"
+import Error from "./component/Error"
 
 function App() {
   return (
-    <div>
-      <Movies />
+    <div className="max-w-9/12 ml-auto mr-auto">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movie/:id" element={<SingleMovie />} />
+          <Route path="*" element={<Error />} />
+        </Routes>  
     </div>
   )
 }
---
+++ src/component/SingleMovie.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+const SingleMovie = () => {
+  const {id} = useParams();
+
+  return (
+    <>
+      {id}
+    </>
+  )
+}
+
+export default SingleMovie
